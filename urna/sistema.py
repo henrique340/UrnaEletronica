25ce75bc476609b21ca1f146dfc883528ceb92e5
henrique340 <Henriqueyuji8@gmail.com>
2023-11-30 12:16:12 -0300

    adicionando sistema.py

---
+++ urna/sistema.py
@@ -0,0 +1,19 @@
+while True:
+    Menu()
+    opc = int(input('Digite sua escolha: '))
+    if opc == 1:
+        Cadastro_Candidatos()
+    elif opc == 2:
+        Cadastrar_Eleitores()
+    elif opc == 3:
+        Votar()
+    elif opc == 4:
+        Resultado()
+    elif opc == 5:
+        Relatorio()
+    elif opc == 6:
+        print('Encerrando o programa')
+        sleep(1)
+        break
+    else:
+        print('Erro, tente novamente')
